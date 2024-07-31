a6667453755fc5468b84399dd08cf27575ab0853
Olexander Pekh <sashapekh@gmail.com>
2024-07-31 11:05:34 +0300

    add terraform infra

---
+++ infra/terraform/main.tf
@@ -0,0 +1,41 @@
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-strapi"
+  location = var.location
+}
+
+resource "azurerm_service_plan" "sp" {
+  name                = "stripesp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  os_type  = "Linux"
+  sku_name = "B1"
+}
+
+
+resource "azurerm_linux_web_app" "app" {
+  name                      = "strapiappsashapekh"
+  resource_group_name       = azurerm_resource_group.rg.name
+  location                  = azurerm_resource_group.rg.location
+  service_plan_id           = azurerm_service_plan.sp.id
+  virtual_network_subnet_id = azurerm_subnet.subnet.id
+
+  site_config {
+    ftps_state = "AllAllowed"
+  }
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "stripesa"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "sc" {
+  name                  = "stripesc"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
